0df53f2b56e84b3afbcaa6018ad77de166b8698d
GRACE\Baby <graceyong1020@gmail.com>
2024-10-30 20:26:31 -0700

    commit

---
+++ src/main/java/com/team4project/Team4ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.team4project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team4ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team4ProjectApplication.class, args);
+    }
+
+}
