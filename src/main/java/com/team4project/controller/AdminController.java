5599081dfe9f900a404b22c9dd8d3ee9f52f25aa
GRACE\Baby <graceyong1020@gmail.com>
2024-10-31 22:12:06 -0700

    타임리프 작업중

---
+++ src/main/java/com/team4project/controller/AdminController.java
@@ -0,0 +1,15 @@
+package com.team4project.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+    @GetMapping("/list")
+    public void List() {
+
+    }
+}
