1d40c50d6ad38c58f1590197541151433273e244
compass149 <compass1351@gmail.com>
2024-11-05 00:07:05 +0900

    테이블 생성 오류 수정, db 연결문자열 주석 처리해서 사용할 것

--- src/main/java/com/team4project/domain/reply/Comment.java
+++ src/main/java/com/team4project/domain/reply/Comment.java
@@ -1,23 +1,24 @@
 package com.team4project.domain.reply;
 
-
 import com.team4project.domain.Board;
 import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Table(name = "Comment", indexes = {@Index(name = "idx_comment_", columnList = "reportBno")})
+@Table(name = "Comment", indexes = {@Index(name = "idx_comment_board", columnList = "board_bno")})
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString(exclude = "board")
 public class Comment {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long rno;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_bno") // 외래 키 열 이름을 명시적으로 지정
     private Board board;
 
     private String commentText;
@@ -26,7 +27,5 @@ public class Comment {
 
     public void changeText(String text) {
         this.commentText = text;
-
-
     }
 }
--- src/main/java/com/team4project/domain/reply/CommunityReply.java
+++ src/main/java/com/team4project/domain/reply/CommunityReply.java
@@ -1,13 +1,11 @@
 package com.team4project.domain.reply;
 
-import com.team4project.domain.Board;
 import com.team4project.domain.CommunityBoard;
 import jakarta.persistence.*;
 import lombok.*;
 
-
 @Entity
-@Table(name = "Reply", indexes = {@Index(name = "idx_comment_", columnList = "communityBno")})
+@Table(name = "Reply", indexes = {@Index(name = "idx_comment_", columnList = "communityId")}) // 컬럼 이름 수정
 @Getter
 @Builder
 @AllArgsConstructor
@@ -20,6 +18,7 @@ public class CommunityReply {
     private Long rno;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "communityId") // 외래 키 이름 설정
     private CommunityBoard communityBoard;
 
     private String replyText;
@@ -28,7 +27,5 @@ public class CommunityReply {
 
     public void changeText(String text) {
         this.replyText = text;
-
-
     }
 }
