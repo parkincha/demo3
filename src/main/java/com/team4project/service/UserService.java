b4e8025b5f5ea4fb30a7961e13fe2c995085b6b2
compass149 <a01055821627@gmail.com>
2024-11-07 18:03:00 +0900

    풀하기위해 커밋

--- src/main/java/com/team4project/service/UserService.java
+++ src/main/java/com/team4project/service/UserService.java
@@ -1,21 +1,4 @@
-//package com.team4project.service;
-//
-//import com.team4project.entity.User;
-//import com.team4project.repository.UserRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.List;
-//
-//@Service
-//public class UserService {
-//    @Autowired
-//    private UserRepository userRepository;
-//
-//    public List<User> findAll() {
-//
-//
-//
-//
+package com.team4project.service;
 
-//user2 계속 쓰실 거면 user2 user 부분 user2로 해서 사용하시면 됩니다.
+public interface UserService {
+}
